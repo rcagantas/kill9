0bda6f6f3bb284a21f3d5b01636060a8f6a7fa0a
Roel Cagantas <roel@plum.gateway.2wire.net>
2014-06-15 14:12:56 +0800

    added first sprite

--- web/gglclient.dart
+++ web/gglclient.dart
@@ -7,22 +7,40 @@ import 'package:stagexl/stagexl.dart';
 class GglClient {
   Stage _stage;
   Map<int, bool> _keyState = new Map<int, bool>();
+  ResourceManager _resMgr;
+
 
   GglClient(this._stage) {
+    drawCircle();
+    _setupResource();
     _setupKeyListener();
   }
-  
+
+  void _setupResource() {
+    _resMgr = new ResourceManager()
+      ..addBitmapData("idle", "./res/man_idle.png");
+    _resMgr.load().then((_) {
+      var idle = new Bitmap(_resMgr.getBitmapData("idle"));
+      idle.setTransform(100, 100);
+      _stage.addChild(idle);
+    });
+
+  }
+
   void drawCircle() {
     var shape = new Shape();
-    shape.graphics.circle(100, 100, 60);
+    shape.graphics.circle(
+        _stage.stageWidth/2,
+        _stage.stageHeight/2,
+        20);
     shape.graphics.fillColor(Color.Red);
     _stage.addChild(shape);
   }
-  
+
   void _setupKeyListener() {
     _stage.focus = _stage;
     _stage.onKeyDown.listen((e) => _keyState[e.keyCode] = true);
     _stage.onKeyUp.listen((e) => _keyState[e.keyCode] = false);
-    _stage.onEnterFrame.listen((e) => print(_keyState));    
+    //_stage.onEnterFrame.listen((e) => print(_keyState));
   }
 }
