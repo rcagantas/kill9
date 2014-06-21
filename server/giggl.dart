7433abf1b23df682baf1bc25ee0633da5111dd5a
happyslurpy <happyslurpy@gmail.com>
2014-06-22 03:01:17 +0800

    movement mechanics
    - cleaned-up WorldObject to make it minimal
    - extended WorldObject as WorldActor that implements the game movement
    mechanic
    - updated demo to use Roel's actor and implement the agreed mechanic
    (WASD+mouse movement)
    - added sprites to server folder (temp only, will remove later)

--- server/giggl.dart
+++ server/giggl.dart
@@ -1,62 +1,68 @@
 import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart';
 import 'giggl_server.dart';
-
+import '../web/giggl.dart';
 void main() {
-  // real world stuff
-  var canvas = html.querySelector('#stage');
-  var stage = new Stage(canvas);
-  var ball = new VisiBall(Color.Blue)
-    ..x = 250
-    ..y = 250;
-  var renderLoop = new RenderLoop();
-  var juggler = renderLoop.juggler;
-  stage.addChild(ball);
-  renderLoop.addStage(stage);
-  
 
-  // ggl server stuff
-  var world = new World(50,50,10);
-  
-  var object1 = new WorldObject(250,250,0,10,100, 2);
-  
-
-  world.addObject(object1);
-
-  // connect real world with ggl
-  object1.eventMoved.addObserver(ball);
- 
-  // .. and presto!
-  world.start();
-  stage.focus = stage;
-  stage.onKeyDown.listen((e) {
-    if (e.keyCode == 37) {
-      object1.turnLeft();
-    }
-    else if (e.keyCode == 38) {
-      object1.moveForward();
-    }
-    else if (e.keyCode == 39) {
-      object1.turnRight();
-    }
-    else if (e.keyCode == 40) {
-      object1.moveBackward();
-    }
+  ResourceHandler.init();
+  resMgr.load().then((_) {
+    // real world stuff
+    var ball = new ListeningActor()
+      ..move(250, 250);
+    var renderLoop = new RenderLoop();
+    var juggler = renderLoop.juggler;
+    stage.addChild(ball);
+    renderLoop.addStage(stage);
     
-  });
   
-  stage.onKeyUp.listen((e) { 
-      if (e.keyCode == 37 || e.keyCode == 39) {
-        object1.stopTurn();
-      }
-      else if (e.keyCode == 38 || e.keyCode == 40) {
-        object1.stopMove();
-      }
-    ;});
+    // ggl server stuff
+    var world = new World(50,50,10);
+    
+    var object1 = new WorldActor(10,200,2)
+      ..x = 250
+      ..y = 250;
+    
   
-  stage.onEnterFrame.listen((EnterFrameEvent e) {
-    html.querySelector('#detail').innerHtml = 'orientation: ${object1.orientation} xV: ${object1.xVelocity} yV: ${object1.yVelocity}';
-  });
+    world.addObject(object1);
+  
+    // connect real world with ggl
+    object1.movementEvent.addObserver(ball);
+   
+    // .. and presto!
+    world.start();
+    stage.focus = stage;
+    stage.onKeyDown.listen((e) {
+      if (e.keyCode == 65) {
+        object1.moveLeft();
+      }
+      else if (e.keyCode == 87) {
+        object1.moveUp();
+      }
+      else if (e.keyCode == 68) {
+        object1.moveRight();
+      }
+      else if (e.keyCode == 83) {
+        object1.moveDown();
+      }
+      
+    });
+    
+    stage.onKeyUp.listen((e) { 
+        if (e.keyCode == 65 || e.keyCode == 68) {
+          object1.stopLefRightMove();
+        }
+        else if (e.keyCode == 83 || e.keyCode == 87) {
+          object1.stopTopDownMove();
+        }
+      ;});
+    
+    stage.onMouseMove.listen((e) { 
+       object1.turnToPoint(e.stageX, e.stageY);
+    });
+    stage.onEnterFrame.listen((EnterFrameEvent e) {
+      html.querySelector('#detail').innerHtml = 'orientation: ${object1.orientation} xV: ${object1.xVelocity} yV: ${object1.yVelocity}';
+    });
+    });
  }
 
 class VisiBall extends Shape implements Observer
@@ -72,9 +78,24 @@ class VisiBall extends Shape implements Observer
     if (event == GglEvent.ObjectMoved) {
       var object = data as WorldObject;
       
-      this.x = object.xLocation;
-      this.y = object.yLocation;
+      this.x = object.x;
+      this.y = object.y;
       this.rotation = object.orientation;
     }
   }
-}  
+}
+
+class ListeningActor extends Actor implements Observer
+{
+  
+  ListeningActor():super();
+  
+  void onNotify (Object data, int event ) {
+    if (event == GglEvent.ObjectMoved) {
+      var object = data as WorldObject;
+      
+      this.move(object.x, object.y);
+      this.rotation = object.orientation;
+    }
+  }
+ }
--- server/object.dart
+++ server/object.dart
@@ -1,101 +1,97 @@
 part of giggl_server;
 
 class WorldObject {
-  num xVelocity;
-  num yVelocity;
-  num xLocation;
-  num yLocation;
-  num turnRate;
-  num orientation;
-  num radius;
-  num movementSpeed;
-  num turnSpeed;
-  bool _forward = true;
   
-  Subject eventMoved = new Subject();
+  // velocity
+  num xVelocity = 0;
+  num yVelocity = 0;
+  num angleVelocity = 0;
+  
+  // position and orientation
+  num x = 0;
+  num y = 0;
+  num radius;
+  num orientation = 0; // orientation in radians, 0 points up, angle expressed clockwise
+  
+  // speed and turn rate (ideally constant for a specific instance of an object)
+  num speed;
+  num turnRate;
+
+  // observable events;
+  Subject movementEvent = new Subject();
   
   final _fullCircle = 2 * PI; 
     
+  WorldObject (this.radius,this.speed, this.turnRate);
   
-  WorldObject (num x, num y, num orientation, num radius, num movementSpeed, num turnSpeed) {
-    xLocation = x;
-    yLocation = y;
-    this.orientation = orientation;
-    this.radius = radius;
-    this.movementSpeed = movementSpeed;
-    this.turnSpeed = turnSpeed;
-   
-    // new object always on standstill;
-    
+  void update(num elapsedTime) {
+  
+    x = x + xVelocity * elapsedTime;
+    y = y + yVelocity * elapsedTime;
+
+    orientation = orientation + angleVelocity * elapsedTime;
+
+    if (orientation > _fullCircle) {
+      orientation = orientation - _fullCircle; 
+    }
+    else if (orientation < 0) {
+      orientation = _fullCircle + orientation;
+    }
+    //if (xVelocity !=0  || yVelocity !=0 || angleVelocity != 0) {
+      movementEvent.notify(this, GglEvent.ObjectMoved);
+    //}
+  }
+}
+
+class WorldActor extends WorldObject 
+{
+
+  WorldActor(num radius,num speed, num turnRate):super(radius, speed, turnRate);
+
+  void moveLeft() {
+    xVelocity = -speed;  
+  }
+  
+  void moveUp () {
+    yVelocity = -speed;
+  }
+  
+  void moveRight() {
+    xVelocity = speed;  
+  }
+  
+  void moveDown () {
+    yVelocity = speed;
+  }
+
+  void stopLefRightMove () {
     xVelocity = 0;
-    yVelocity = 0;
-    turnRate = 0;
   }
   
-  void moveForward() {
-    xVelocity = movementSpeed * sin(orientation);
-    yVelocity = - movementSpeed * cos(orientation);
-    _forward = true;
-  }
-  
-  void moveBackward() {
-    xVelocity = - movementSpeed * sin(orientation);
-    yVelocity = movementSpeed * cos(orientation);
-    _forward = false;
-  }
-    
-  void stopMove() {
-    xVelocity = 0;
+  void stopTopDownMove () {
     yVelocity = 0;
   }
   
-  void turnLeft() {
-    turnRate = -turnSpeed;
+  void turnClockwise() {
+    angleVelocity = turnRate;
   }
- 
-  void turnRight() {
-    turnRate = turnSpeed;
+  
+  void turnCounterClockise() {
+    angleVelocity = -turnRate;  
+  }
+  
+  void turnToPoint(num xPt,num yPt) 
+  {
+    orientation = PI - atan2(xPt-x, yPt-y);
   }
   
   void stopTurn() {
-       turnRate = 0;
-  }
-    
-  
-  bool isMoving() {
-    return !(xVelocity == 0 && yVelocity == 0);
+    angleVelocity = 0;  
   }
   
-  bool isTurning() {
-    return turnRate != 0;
-  }
   void update(num elapsedTime) {
-  
-    if (isMoving()) {
-      // recalculate based on orientation
-      if (_forward) {
-        moveForward();
-      }
-      else {
-        moveBackward();
-      }
-      xLocation = xLocation + xVelocity * elapsedTime;
-      yLocation = yLocation + yVelocity * elapsedTime;
-    }
-    
-    if (isTurning()) {
-      orientation = orientation + turnRate * elapsedTime;
-
-      if (orientation > _fullCircle) {
-        orientation = orientation - _fullCircle; 
-      }
-      else if (orientation < 0) {
-        orientation = _fullCircle + orientation;
-      }
-    }
-    
-    if (isMoving() || isTurning()) {
-      eventMoved.notify(this, GglEvent.ObjectMoved);
-    }
+    // do some funky stuff here
+    super.update(elapsedTime);
   }
+
 }
