451eb80248173094af0b99606b0bfa8030ccde71
Roel Cagantas <roel.cagantas@gmail.com>
2014-06-25 22:44:42 +0800

    moved non client code to test_client

--- web/client/client.dart
+++ web/client/client.dart
@@ -7,9 +7,8 @@ part of giggl;
  *
  */
 class Client extends DisplayObjectContainer {
-  Actor p1, p2;
-  InputHandler kb = new InputHandler()
-    ..propagate = false;
+  List<Actor> actors = [];
+  Actor p1;
 
   Client() {
     ResourceHandler.init();
@@ -25,22 +24,6 @@ class Client extends DisplayObjectContainer {
 
       stage.addChild(this);
       renderLoop.addStage(stage);
-      stage.onEnterFrame.listen(onFrame);
     });
-  }
-
-  void onFrame(Event e) {
-    if (p1 == null) return;
-    num ix = 0, iy = 0, ih = 0, it = 0, inc = 2, rinc = 0.1;
-    if (kb.keyState[87]) { iy = inc; }
-    if (kb.keyState[83]) { iy = -inc; }
-    if (kb.keyState[65]) { ix = inc; }
-    if (kb.keyState[68]) { ix = -inc; }
-    if (kb.keyState[37]) { it = -rinc; }
-    if (kb.keyState[39]) { it = rinc; }
-    if (kb.keyState[40]) { p1.cycleWeapon(); }
-
-    p1.move(p1.x - ix, p1.y - iy);
-    p1.torsoRotate(it);
   }
 }
--- web/test_client.dart
+++ web/test_client.dart
@@ -1,5 +1,35 @@
 import 'giggl.dart';
+import 'package:stagexl/stagexl.dart';
+
+InputHandler kb;
+Client client;
 
 void main() {
-  new Client();
+  kb = new InputHandler();
+  client = new Client();
+  stage.onKeyUp.listen(onKeyUp);
+  stage.onEnterFrame.listen(onFrame);
+}
+
+
+void onFrame(Event e) {
+  Actor p1 = client.p1;
+  if (p1 == null) return;
+  num ix = 0, iy = 0, ih = 0, it = 0, inc = 2, rinc = 0.1;
+  if (kb.keyState[87]) { iy = inc; }
+  if (kb.keyState[83]) { iy = -inc; }
+  if (kb.keyState[65]) { ix = inc; }
+  if (kb.keyState[68]) { ix = -inc; }
+  if (kb.keyState[37]) { it = -rinc; }
+  if (kb.keyState[39]) { it = rinc; }
+  p1.move(p1.x - ix, p1.y - iy);
+  p1.torsoRotate(it);
+}
+
+void onKeyUp(KeyboardEvent event) {
+  Actor p1 = client.p1;
+  if (p1 == null) return;
+  if (event.keyCode == 40) { p1.cycleWeapon(); }
+  if (event.keyCode == 39 || event.keyCode == 40)
+    event.stopImmediatePropagation();
 }
