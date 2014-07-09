d41c67ee3d45e260a556fb7489506a658debffb2
asantiola <asantiola@gmail.com>
2014-07-09 18:17:44 +0800

    initial ver
    initial ver

---
+++ lib/network/gameclient.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+void main() {
+  WebSocket socket = new WebSocket('ws://127.0.0.1:1024/ws');
+  
+  if (socket != null && socket.readyState == WebSocket.OPEN) {
+    socket.send('connecting');
+  }
+  
+  socket.onMessage.listen((e) => datareceived(e));
+}
+
+void datareceived(MessageEvent event) {
+  querySelector("#txt1").text = event.data;
+}
---
+++ lib/network/gameserver.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+import 'dart:async';
+//import 'dart:math';
+
+class GameServer {
+  String _hostname;
+  int _port;
+  var _serverListener;
+  var _serverWriter;
+  Map<int,WebSocket> _mapClients = new Map();
+  Timer pinger = null; 
+  
+  GameServer(String hostname, int port, var addClient, var serverListener, var serverWriter) {
+    _hostname = hostname;
+    _port = port;
+    _serverListener = serverListener;
+    _serverWriter = serverWriter;
+    
+    // each Game server starts a new Host
+    HttpServer
+      .bind(hostname, port)
+      .then((server) {
+        server.listen((req) {
+          if (req.uri.path == '/ws') {
+            WebSocketTransformer.upgrade(req).then((websocket) {
+              // getId for client and add to map
+              int clientId = addClient();
+              _mapClients[clientId] = websocket;
+              
+              _mapClients[clientId].listen(_readMessage);
+              
+              // inform client of its clientId
+              String msg = 'clientId=$clientId';
+              _mapClients[clientId].add(msg);
+            });
+          }
+        });
+      });
+      // add catchError!
+    
+      // ticker to writeout message, every duration tick
+      if (pinger == null) {
+        pinger = new Timer(new Duration(seconds: 10), _writeMessage);
+      }
+  }
+  
+  void _readMessage(String msg) {
+    // do parsing and pass to callback function
+    //_serverListener(data);
+    
+    print('debug received $msg');
+  }
+  
+  void send(int clientId, var data) {
+    //convert data to String
+    //_writeMessage(_mapClients[clientId], msg);
+  }
+  
+  void _writeMessage() {
+    // Loop thru all clients, and try to send a data
+    //_serverWriter(data)
+    //_mapClients[clientId].add(msg);
+    
+    _mapClients.forEach((clientId,socket) {
+      //String msg = _serverWriter(clientId);
+      String msg = 'hello $clientId';
+      _mapClients[clientId].add(msg);
+      print('debug sending $msg');
+    });
+  }
+}
+
+int index = 0;
+
+// debugging only
+void main() {
+  GameServer gs1 = new GameServer('127.0.0.1', 1024, addPlayer, serverListener, serverWriter);
+  //GameServer gs2 = new GameServer('127.0.0.1', 1025, addPlayer, serverListener, serverWriter);
+}
+
+int addPlayer() {
+  return ++index;
+}
+
+void serverListener() {
+}
+
+String serverWriter() {
+  return 'test';
+}
