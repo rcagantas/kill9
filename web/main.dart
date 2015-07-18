bb81be222747b257311459356c782431ec67803a
Roel Cagantas <roel.cagantas@gmail.com>
2015-07-18 21:55:56 +0800

    remove unused assets

---
+++ web/main.dart
@@ -0,0 +1,80 @@
+import 'dart:html';
+import 'dart:async';
+import 'dart:convert';
+import 'package:stagexl/stagexl.dart';
+import 'package:giggl/gglclient2.dart';
+import 'package:giggl/gglcommon.dart';
+
+
+class GameClient extends DisplayObjectContainer {
+  WebSocket ws;
+  Arena arena;
+  HudLayer hud;
+  bool reconnecting = false;
+  bool connected = false;
+
+  GameClient() {
+    initWebSocket();
+  }
+
+  void initWebSocket() {
+    ws = new WebSocket("ws://127.0.0.1:4040/ws");
+    ws.onOpen.listen((e) { connected = true; });
+    ws.onClose.listen((e) { tryReconnect(); });
+    ws.onError.listen((e) { tryReconnect(); });
+    ws.onMessage.listen((e) {
+      if (!connected) return;
+      if (arena == null) {
+        num w = 20, h = 20;
+        List<num> grid = JSON.decode(e.data);
+        arena = new Arena(w, h, grid)..addTo(this);
+        hud = new HudLayer(w, h, grid)..addTo(this);
+      } else {
+        Frame pf = new Frame.fromString(e.data);
+        arena.updateFrame(pf);
+        hud.updateFrame(pf);
+      }
+    });
+    reconnecting = false;
+    debugWindow.serverMessage = "connected.";
+  }
+
+  void tryReconnect() {
+    connected = false;
+    if (reconnecting) return;
+    debugWindow.serverMessage = "reconnecting...";
+    reconnecting = true;
+    new Timer(new Duration(milliseconds: 1000), () => initWebSocket());
+  }
+
+  void action(Cmd c) {
+    c.id = c.hashCode;
+    c.name = "Socket Test";
+    if (ws != null && ws.readyState == WebSocket.OPEN) {
+      ws.send(c.toData());
+    }
+  }
+}
+
+void setupStage(Map stages, String s) {
+  input.removeAllListeners();
+  stage.removeChildren();
+  stage.addChild(stages[s]);
+  input.addListener(stages[s].action);
+  stage.addChild(debugWindow);
+}
+
+void main() {
+  //initialize global variables
+  RenderLoop renderLoop = new RenderLoop();
+  renderLoop.addStage(stage);
+
+  Map stages = new Map();
+
+  resourceLoader.load();
+  resource.load().then((_) {
+    input.init();
+    stages.putIfAbsent("socket", () { return new GameClient(); });
+    setupStage(stages, "socket");
+  });
+}
